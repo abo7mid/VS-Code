292a8ed36ed65522ec88535c2313491e41b53008
Ahmed Nadawy <a.anadawy@gmail.com>
2022-06-05 21:19:22 +0300

    Python

---
+++ python_stack/_python/python_fundamentals/Functions_Basic_I.py
@@ -0,0 +1,138 @@
+#1 
+#prediction  print 5
+
+def a():
+    return 5
+print(a())
+
+
+#2
+#prediction  print 10
+def a():
+    return 5
+print(a()+a())
+
+# #3
+#prediction  print 5
+def a():
+    return 5
+    return 10
+print(a())
+
+# #4
+#prediction  print 5
+
+def a():
+    return 5
+    print(10)
+print(a())
+
+# #5
+#prediction  print 5
+
+def a():
+    print(5)
+x = a()
+print(x)  # I did not predict it will print None
+
+# #6
+#prediction  print 8
+# def a(b,c):
+#     print(b+c)
+# print(a(1,2) + a(2,3))
+
+# #7
+#prediction  print 25
+
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+
+# #8
+#prediction  print 100
+def a():
+    b = 100
+    # print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a())
+
+# #9
+#prediction  print 21
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))
+
+# #10
+#prediction  print 8
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5))
+
+# #11 #prediction  print 500,300,500,300,500
+
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)
+
+# #12
+#prediction  print 500,500,300,500
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+a()
+print(b)
+
+# #13
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=a()
+print(b)
+
+# #14
+#prediction  print 1 3 2
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+
+# # 15 
+# # prediction  print 1 3 5 10
+
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
