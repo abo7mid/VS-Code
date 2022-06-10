36ee1a5e5e5d2d6fff56b0cdab904dcaf9658365
Ahmed Nadawy <a.anadawy@gmail.com>
2022-06-10 12:05:01 +0300

    Playground

---
+++ python_stack/_python/flask/flask_fundamentals/hello_flask/Understanding_Routing.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/dojo')
+
+def dojo():
+    return "Dojo!"
+
+
+@app.route('/say/<name>')
+
+def say(name):
+    return f"Hi  {name}!" 
+
+
+
+@app.route('/repeat/<int:times>/<word>')
+def checkInt(times,word):
+        print(word*int(times))
+        return f"{word} " * int(times)
+
+
+@app.route('/repeat/<string:times>/<word>')
+def checkStr(times,word):
+    return "must be an integer"
+
+
+@app.errorhandler(404)
+def page_not_Found(e):
+    print("404")
+    return "Sorry! No response. Try again."
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ python_stack/_python/flask/flask_fundamentals/hello_flask/playground.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, get_template_attribute
+
+app = Flask(__name__)
+
+@app.route("/play")
+def play():
+    return render_template("play1.html")
+
+
+
+
+@app.route("/play/<int:times>")
+def playTimes(times):
+    return render_template("play2.html",times=int(times))
+    # what if we want to render the same page
+    # NINJA BONUS: Use only one template for the whole project, HOOOOOW?
+
+
+
+
+@app.route("/play/<int:times>/<string:color>")
+def playTimesAndColor(times,color):
+    return render_template("play3.html",times=int(times),color=color)
+
+
+
+
+
+@app.errorhandler(404)
+def NotFound(e):
+    return "Page Not Found"
+
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
