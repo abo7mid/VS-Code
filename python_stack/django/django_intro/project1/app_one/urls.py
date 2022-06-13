9d27c97ce4691774505f43c2f07f8ab02d5f9101
Ahmed Nadawy <a.anadawy@gmail.com>
2022-06-13 22:24:58 +0300

    Django Project1

--- python_stack/django/django_intro/project1/app_one/urls.py
+++ python_stack/django/django_intro/project1/app_one/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path     
+from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    # views.login is a function gets invoked whenever someones go to the route '/login'
+    path('login', views.login),
+    # use <str:name> whenever you want to pass a string in the route '/' ex /John
+    # path('<str:name>',views.index), 
+
+    path('another_route',views.another_method),
+    path('redirected_route',views.redirected_method)
+    
 ]
--- python_stack/django/django_intro/project1/app_one/views.py
+++ python_stack/django/django_intro/project1/app_one/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render, HttpResponse
-def index(request):
+from django.shortcuts import render, HttpResponse ,redirect
+from django.http import JsonResponse
+
+
+# this function is invoked by whoever request '/' route
+# the name parameter is passed using path function in urls module
+# the request parameter: Every function's first argument will be the request object
+def index(request,name):  
     return HttpResponse("this is the equivalent of @app.route('/')!")
+
+
+def login(request): # this function is invoked by whoever requests '/login' route, see urls
+    return HttpResponse("this is the equivalent of @app.route('/login')!")
+
+
+# when redirecting, you should provide the whole path, starting with the first /.
+def another_method(request):
+    return redirect("/redirected_route")
+
+
+def redirected_method(request):
+    return JsonResponse({"response":"JSON response from redirected_method","status":True})
+
+
+    # path('another_route',views.another_method),
+    # path('redirected_route',views.redirected_method)
+    
