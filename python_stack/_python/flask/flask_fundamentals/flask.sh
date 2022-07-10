83e1716a2b3ffc65d253a30e740edbc7c923cbd1
Ahmed Nadawy <a.anadawy@gmail.com>
2022-07-11 00:14:04 +0300

    create flask project

--- python_stack/_python/flask/flask_fundamentals/flask.sh
+++ python_stack/_python/flask/flask_fundamentals/flask.sh
@@ -1,5 +1,8 @@
-path='$HOME/Assignments/python_stack/_python/flask/flask_fundamentals'
+path="$HOME/Assignments/python_stack/_python/flask/flask_fundamentals"
 cd $path
+
+
+if [ ! -d $1 ]; then
 mkdir $1
 cd $1
 touch server.py
@@ -9,36 +12,34 @@ touch templates/base.html
 touch templates/index.html
 
 
-
-
-base='<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    {%block head%}{%endblock%}
-</head>
-<body>
-    {%block navbar%}{%endblock%}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <div class="container">
-        {%block content%}{%endblock%}
-    </div>
-</body>
+base='<!DOCTYPE html>\n
+<html lang="en">\n
+<head>\n
+    <meta charset="UTF-8">\n
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">\n
+    {%block head%}\n\n{%endblock%}\n
+</head>\n
+<body>\n
+    {%block navbar%}\n\n{%endblock%}\n
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>\n
+    <div class="container">\n
+        {%block content%}\n\n{%endblock%}\n
+    </div>\n
+</body>\n
 </html>'
 
-index="{% extends 'base.html'%}
-{%block head%}{%endblock%}
-{%block navbar%}{%endblock%}
-{%block content%}
-<h1>test</h1>
-{%endblock%}"
+index="{% extends 'base.html'%}\n
+{%block head%}\n\n{%endblock%}\n
+{%block navbar%}\n\n{%endblock%}\n
+{%block content%}\n
+<h1>test</h1>\n
+{%endblock%}\n"
 
 
-echo $index > templates/index.html
-echo $base > templates/base.html
+echo -e $index > templates/index.html
+echo -e $base > templates/base.html
 
 
 serverCode="from flask import Flask, render_template\n\n\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n\treturn render_template('index.html')\napp.run(debug=True)"
@@ -48,4 +49,9 @@ echo -e $serverCode > server.py
 
 ## copy mysqlconnection class to current project
 
-cp $path/first_flask_mysql/mysqlconnection.py .
+#cp $path/first_flask_mysql/mysqlconnection.py .
+
+cp $HOME/Assignments/python_stack/_python/flask/flask_fundamentals/first_flask_mysql/mysqlconnection.py .
+else
+	echo "project with same name exists! "
+fi
