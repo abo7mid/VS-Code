2a1551927ad206738486d6a2365fede58063aaf2
Ahmed Nadawy <a.anadawy@gmail.com>
2022-07-17 06:47:09 +0300

    Java Stack

---
+++ java_stack/Aboutme.java
@@ -0,0 +1,7 @@
+public class Aboutme{
+ public static void main(String[] args){
+ System.out.println("My name is Ahmed");
+ System.out.println("I'm old enough to code :)");
+ System.out.println("My hometown is Makkah, SA");
+}
+}
