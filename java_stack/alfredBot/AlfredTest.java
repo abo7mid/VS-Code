34ed799264910cc2a267f6c832b02b2502471e47
Ahmed Nadawy <a.anadawy@gmail.com>
2022-07-21 22:58:00 +0300

    Alfred Bot

---
+++ java_stack/alfredBot/AlfredTest.java
@@ -0,0 +1,10 @@
+public class AlfredTest{
+
+    public static void main(String[] args){
+        AlfredQuotes alfredBot = new AlfredQuotes();
+        System.out.println(alfredBot.guestGreeting("Beth Kane"));
+        System.out.println(alfredBot.dateAnnouncement());
+        System.out.println(alfredBot.respondBeforeAlexis("Alfred! Play some low-fi beats."));
+
+    }
+}
