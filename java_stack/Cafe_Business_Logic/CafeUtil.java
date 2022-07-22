e858f08a8b5777a7a259487ebe241346ba12c9f7
Ahmed Nadawy <a.anadawy@gmail.com>
2022-07-22 20:18:29 +0300

    Cafe Business Logic

---
+++ java_stack/Cafe_Business_Logic/CafeUtil.java
@@ -0,0 +1,64 @@
+package Cafe_Business_Logic;
+
+import java.util.ArrayList;
+
+
+public class CafeUtil{
+    
+int getStreakGoal(){
+    int sums = 0;
+    for(int i = 0;i<=10;i++){
+        sums = sums + i;
+    }
+    return sums;
+}
+
+int getStreakGoal(int numWeeks){
+    int sums = 0;
+    for(int i = 0;i<=numWeeks;i++){
+    sums = sums + i;
+    }
+    return sums;
+}
+
+double getOrderTotal(double[] prices){
+    double total = 0;
+    for(int i = 0;i<prices.length;i++){
+    total = total + prices[i];
+        
+    }
+    return total;
+}
+
+void displayMenu(ArrayList<String> menuItems){
+    // for(String menuitem: menuItems){
+    //     System.out.println(menuitem);
+    // }
+    for(byte i = 0;i<menuItems.size();i++ ){
+        System.out.println(String.format("%s %s",i, menuItems.get(i)));
+    }
+    
+}
+
+
+void addCustomer(ArrayList<String> customers){
+    System.out.println("Please enter your name:");
+    String name = System.console().readLine();
+    System.out.println("Hello, "+ name);
+    System.out.println(String.format("There are %s people in front of you ",customers.size()));
+    customers.add(name);
+    System.out.println(customers);
+    
+    
+}
+
+void printPriceChart(String product, double price,int maxQuantity){
+    System.out.println(product);
+    for(int quantity =1;quantity<=maxQuantity;quantity++){
+        System.out.println(String.format("%s - $%s",quantity,price*quantity));
+        
+    }
+    
+}
+    
+}
---
+++ java_stack/Cafe_Business_Logic/TestCafe.java
@@ -0,0 +1,33 @@
+package Cafe_Business_Logic;
+
+import java.util.ArrayList;
+
+public class TestCafe {
+    public static void main(String[] args) {
+        CafeUtil cafe = new CafeUtil();
+        System.out.println("\n----- Streak Goal Test -----");
+        System.out.printf("Purchases needed by week 10: %s \n\n", cafe.getStreakGoal(11));
+
+        System.out.println("----- Order Total Test-----");
+        double prices[] = { 5.2, 2.5, 4.9 };
+        System.out.printf("Order total: %s \n\n", cafe.getOrderTotal(prices));
+
+        System.out.println("----- Display Menu Test-----");
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        cafe.displayMenu(menu);
+
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        for(int i = 0;i<4;i++){
+
+        cafe.addCustomer(customers);
+        }
+
+        System.out.println("\n----- Ninja Bonuses-----");
+        cafe.printPriceChart("coffee", 22.5, 3);
+    }
+}
